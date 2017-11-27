b690bb349c650590979634a82e54b883439e68dd
Bee <pchatkun@gmail.com>
2017-11-28 00:22:29 +0700

    add2

--- App.js
+++ App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  jsondata() {
+      return (
+        fetch('https://github.com/srpbbee/Hotel/blob/master/hotel.json')
+            .then(res => res.json())
+            .then((out) => {
+                console.log(out);
+        }).catch(err => console.error(err));
+      );
+    }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      jsondata()
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
