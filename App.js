c8ccbf3195d7d5c989a3569daae711a7ed8fb633
Bee <pchatkun@gmail.com>
2017-11-28 00:30:07 +0700

    Add3

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
 
   jsondata() {
       return (
-        fetch('https://github.com/srpbbee/Hotel/blob/master/hotel.json')
+        fetch('https://raw.githubusercontent.com/srpbbee/Hotel/master/hotel.json')
             .then(res => res.json())
             .then((out) => {
                 console.log(out);
